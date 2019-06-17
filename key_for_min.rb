eae32d7b101a2179c2fc1d1726ce5bad04833344
Samantha Kane <s.kane133@gmail.com>
2019-06-17 02:45:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,10 @@ def key_for_min_value(name_hash)
   min_k = nil
   min_v = nil 
 name_hash.each do |k, v|
-  if min_v > v
+  if min_v == nil
+    min_k = k 
+    min_v = v
+  elsif min_v > v
     min_k = k 
     min_v = v
  end
